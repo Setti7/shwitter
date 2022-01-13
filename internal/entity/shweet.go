5577eb56641cc13cdb7eabd8f01b81c4acf0ce57
André Niero Setti <ansetti7@gmail.com>
2022-01-12 21:23:03 -0300

    Fix timeline and userline order

--- internal/entity/shweet.go
+++ internal/entity/shweet.go
@@ -3,7 +3,7 @@ package entity
 import "time"
 
 type Shweet struct {
-	ID        string    `json:"id"` // TODO:change schema (not here) to timeuuid
+	ID        string    `json:"id"`
 	UserID    string    `json:"-"`
 	Message   string    `json:"message"`
 	User      *User     `json:"user,omitempty"`
